f5a1c9eceed5d6c98757bb5f3b644c98a7944c48
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 20:51:25 -0300

    Aula 04-03 - finalizando app

--- app/src/main/java/br/com/fiap/chatap/ChatActivity.kt
+++ app/src/main/java/br/com/fiap/chatap/ChatActivity.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.chatap
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.fiap.chatap.databinding.ActivityChatBinding
@@ -12,5 +13,47 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupButtons()
+        binding.textInputEditTextMessage.setText(
+            // pega o valor que está vindo da action de origem (outro aplicativo)
+            intent.extras?.getString(Intent.EXTRA_TEXT) /* ?: "Olá" (se vier null, setar como padrão */
+        )
+    }
+
+    private fun setupButtons() {
+        binding.buttonSendMessage.setOnClickListener() {
+            binding.messageValue.text = binding.textInputEditTextMessage.text
+            // limpa o valor do input
+            clearText()
+        }
+
+        binding.buttonShareMessage.setOnClickListener() {
+            /** quando o usuáro clicar em compartilhar ele vai criar um objeto
+             * intent  com as informações necessárias para ser compartilhada
+             * com outra activity de outro aplicativo. */
+            val sendIntent: Intent = Intent().apply {
+                // action de envio par aoutro app
+                action = Intent.ACTION_SEND
+                // tipo de envio (no caso desse app uma string)
+                type = "text/plain"
+                // aqui vem o valor à ser enviado
+                putExtra(
+                    // variável que será usada pelos outros apps para identificar nossa mensagem
+                    Intent.EXTRA_TEXT,
+                    // valor da mensagem que o usuário digitou no input
+                    binding.messageValue.text.toString()
+                )
+            }
+            // cria um objeto para ser enviado para a outra activity
+            val shareIntent = Intent.createChooser(
+                sendIntent,
+                getString(R.string.chooser_title)
+            )
+            startActivity(shareIntent)
+        }
+    }
+
+    private fun clearText() {
+        binding.textInputEditTextMessage.text?.clear();
     }
 }
